bd0d84271e0b6e3d7131ff66f29b4209befb04ab
Nathan Walker <nathanwalker5@gmail.com>
2021-03-21 17:09:18 -0400

    second commit

--- sql/employeetracker_db.sql
+++ sql/employeetracker_db.sql
@@ -4,13 +4,13 @@ CREATE DATABASE employeetracker_db;
 USE employeetracker_db;
 
 CREATE TABLE department (
-id INTEGER,
+id INTEGER AUTO_INCREMENT,
 name VARCHAR(30),
 PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-id INTEGER,
+id INTEGER AUTO_INCREMENT,
 title VARCHAR(30),
 salary DECIMAL,
 department_id INTEGER,
@@ -18,7 +18,7 @@ PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
-id INTEGER,
+id INTEGER AUTO_INCREMENT,
 first_name VARCHAR(30),
 last_name VARCHAR(30),
 role_id INTEGER,
---
+++ sql/seed.sql
@@ -0,0 +1,9 @@
+INSERT INTO department (name)
+VALUES ("Human Resources");
+INSERT INTO department (name)
+VALUES ("Accounting/Finance");
+INSERT INTO department (name)
+VALUES ("Sales");
+INSERT INTO department (name)
+VALUES ("IT");
+
